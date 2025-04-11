425c27a379c397c3f7163e4e7f91b7dd932d9459
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-04-11 10:52:30 +0530

    feat: setup session routes

--- backend/routes/sessionRouters.js
+++ backend/routes/sessionRouters.js
@@ -1,5 +1,12 @@
 const { Router } = require("express");
+const {
+    signInAUser,
+    signOutAUser,
+} = require("../controllers/sessionControllers");
 
 const sessionRouter = Router();
+
+sessionRouter.post("/", signInAUser);
+sessionRouter.delete("/", signOutAUser);
 
 module.exports = sessionRouter;
