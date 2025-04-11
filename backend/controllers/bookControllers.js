035918e0f2afcd3399371930baf337b2021c3863
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-04-11 11:43:38 +0530

    feat: write CRUD controller logics for books

--- backend/controllers/bookControllers.js
+++ backend/controllers/bookControllers.js
@@ -7,6 +7,58 @@ const getAllBooks = asyncHandler(async (req, res) => {
     res.status(200).json(allBooks);
 });
 
+const createANewBook = asyncHandler(async (req, res) => {
+    const newBookData = req.body;
+
+    const newBook = await prismaClient.book.create({
+        data: newBookData,
+    });
+
+    res.status(200).json(newBook);
+});
+
+const getABookById = asyncHandler(async (req, res) => {
+    const bookId = req.params.bookId;
+
+    const book = await prismaClient.book.findUnique({
+        where: {
+            id: bookId,
+        },
+    });
+
+    res.status(200).json(book);
+});
+
+const updateABook = asyncHandler(async (req, res) => {
+    const updatedBookId = req.params.bookId;
+    const updatedBookData = req.body;
+
+    const updatedBook = await prismaClient.book.update({
+        where: {
+            id: updatedBookId,
+        },
+        data: updatedBookData,
+    });
+
+    res.status(200).json(updatedBook);
+});
+
+const deleteABook = asyncHandler(async (req, res) => {
+    const bookId = req.params.bookId;
+
+    const deletedBook = await prismaClient.book.delete({
+        where: {
+            id: bookId,
+        },
+    });
+
+    res.status(200).json(deletedBook);
+});
+
 module.exports = {
     getAllBooks,
+    createANewBook,
+    getABookById,
+    updateABook,
+    deleteABook,
 }
