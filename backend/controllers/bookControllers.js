fb01a4f61db702faac9548056e944d95c4a49a43
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-04-11 09:32:14 +0530

    feat: write controller logic to get all books

--- backend/controllers/bookControllers.js
+++ backend/controllers/bookControllers.js
@@ -1,7 +1,10 @@
 const asyncHandler = require("express-async-handler");
+const { prismaClient } = require("../prisma/client");
 
 const getAllBooks = asyncHandler(async (req, res) => {
-    
+    const allBooks = prismaClient.book.findMany();
+
+    res.status(200).json(allBooks);
 });
 
 module.exports = {
