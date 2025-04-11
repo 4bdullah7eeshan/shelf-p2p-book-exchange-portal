2eee0d10c06da78f70c27a19e60970b1ea8d048e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-04-11 11:46:40 +0530

    feat: define routes for CRUD book operations

--- backend/routes/bookRouters.js
+++ backend/routes/bookRouters.js
@@ -1,9 +1,19 @@
 const { Router } = require("express");
 const { getBooks } = require("../contexts/bookContexts");
+const {
+    createANewBook,
+    getABookById,
+    updateABook,
+    deleteABook,
+} = require("../controllers/bookControllers");
 
 const bookRouter = Router();
 
 bookRouter.get("/", getBooks);
+bookRouter.get("/:bookId", getABookById);
+bookRouter.post("/", createANewBook);
+bookRouter.put("/:bookId", updateABook);
+bookRouter.delete("/:bookId", deleteABook);
 
 
 module.exports = bookRouter;
