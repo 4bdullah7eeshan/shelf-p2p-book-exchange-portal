4ad491fc65a75f26c92ba7e0b565a9a3a987ef55
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-04-12 21:04:45 +0530

    fix: configure dotenv without path

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,13 +1,6 @@
 const dotenv = require("dotenv");
-const path = require("path");
 
-const result = dotenv.config({
-    path: path.resolve(__dirname, '..', '.env'),
-});
-
-if (result.error) {
-    throw result.error;
-}
+dotenv.config();
 
 module.exports = {
     PORT: process.env.PORT || 3000,
