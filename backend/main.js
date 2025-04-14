f0b54ba9f6fb6ace2cd1696309e72a7707b750d3
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-04-14 15:04:01 +0530

    feat: change server to http and setup socket.io

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,32 @@
+const { createServer } = require("node:http");
+const { Server } = require('socket.io');
 const { PORT, NODE_ENV } = require("./config/index");
 const app = require("./app");
+
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true
+    },
+    connectionStateRecovery: {
+
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('A user has connected');
+
+    socket.on('register', (userId) => {
+        socket.join(`user_${userId}`);
+        console.log(`User ${userId} joined room user_${userId}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
 if (NODE_ENV != "test") {
     app.listen(PORT, () => {
